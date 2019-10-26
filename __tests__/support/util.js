09ffef84cb377a68f505fb75397a7edc0c46baa2
shufo <meikyowise@gmail.com>
2019-10-26 22:58:41 +0900

    Copy file in sync mode

--- __tests__/support/util.js
+++ __tests__/support/util.js
@@ -2,9 +2,7 @@ const path = require('path');
 const fs = require('fs-extra');
 
 function populateFixtures(targetDir) {
-  fs.copy(path.resolve(__basedir, '__tests__', 'fixtures'), targetDir).catch(
-    err => console.error(err),
-  );
+  fs.copySync(path.resolve(__basedir, '__tests__', 'fixtures'), targetDir);
 }
 
 module.exports = { populateFixtures };
