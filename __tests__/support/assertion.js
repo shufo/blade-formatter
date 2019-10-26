de4da83dc8d4fe26f83755ff9a632293b3939e49
shufo <meikyowise@gmail.com>
2019-10-26 22:58:13 +0900

    Check if target file is exists

--- __tests__/support/assertion.js
+++ __tests__/support/assertion.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const path = require('path');
 const cmd = require('./cmd');
 
@@ -16,6 +17,13 @@ function assertFormatted(file) {
 }
 
 function assertNotFormatted(file) {
+  fs.exists(file, exists => {
+    if (!exists) {
+      process.stdout.write('Format target file not found\n');
+      process.exit(1);
+    }
+  });
+
   const response = cmd.executeSync(
     path.resolve(__basedir, 'bin', 'blade-formatter'),
     [file, '-c'],
