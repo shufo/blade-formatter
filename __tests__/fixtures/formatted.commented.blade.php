97ea95712e691261d2f97a1a96220aa34908fdfa
shufo <meikyowise@gmail.com>
2020-07-29 19:23:55 +0900

    refactor: 💡 format to new formatting rules

--- __tests__/fixtures/formatted.commented.blade.php
+++ __tests__/fixtures/formatted.commented.blade.php
@@ -1,11 +1,11 @@
-{{--@foreach($downloadOptions as $d => $dO)
+{{--@foreach ($downloadOptions as $d => $dO)
 <tr>
 <td>{{ $dO['description'] }}</td>
 <td>{{ $dO['cost'] }}</td>
 <td>
 <form method="POST" action="{{ route('bonusexchange', ['id' => $dO['id']]) }}">
-    @csrf
-    <button type="submit" class="btn btn-sm btn-info btn-exchange">@lang('bon.exchange')</button>
+@csrf
+<button type="submit" class="btn btn-sm btn-info btn-exchange">@lang('bon.exchange')</button>
 </form>
 </td>
 </tr>
