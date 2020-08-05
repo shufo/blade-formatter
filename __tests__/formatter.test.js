a88ca45b8700216a86b6fa7e425c5db99d59f0cc
shufo <meikyowise@gmail.com>
2020-08-05 15:29:32 +0900

    test: 💍 mixed html opening tag and directive should work
    fixes #5

--- __tests__/formatter.test.js
+++ __tests__/formatter.test.js
@@ -1026,4 +1026,40 @@ describe('formatter', () => {
       assert.equal(result, expected);
     });
   });
+
+  test('mixed html tag and directive #5', async () => {
+    const content = [
+      `@extends('dashboard')`,
+      ``,
+      `@section('content')`,
+      `@if( $member->isAdmin() )`,
+      `<div class="focus">`,
+      `@endif`,
+      `<span>Test!</span>`,
+      `@if( $member->isAdmin() )`,
+      `</div>`,
+      `@endif`,
+      `@endsection`,
+      ``,
+    ].join('\n');
+
+    const expected = [
+      `@extends('dashboard')`,
+      ``,
+      `@section('content')`,
+      `    @if ($member->isAdmin())`,
+      `        <div class="focus">`,
+      `    @endif`,
+      `    <span>Test!</span>`,
+      `    @if ($member->isAdmin())`,
+      `        </div>`,
+      `    @endif`,
+      `@endsection`,
+      ``,
+    ].join('\n');
+
+    return new BladeFormatter().format(content).then((result) => {
+      assert.equal(result, expected);
+    });
+  });
 });
