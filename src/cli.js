389ce0a91a98d64839ab99ae216975d55daeb04f
slovenianGooner <lovro.papez@gmail.com>
2020-10-16 20:11:39 +0200

    Change parenthesis

--- src/cli.js
+++ src/cli.js
@@ -49,7 +49,7 @@ export default async function cli() {
       alias: 'wrap-atts',
       type: 'string',
       description: 'The way to wrap attributes',
-      default: "auto",
+      default: 'auto',
     })
     .option('progress', {
       alias: 'p',
