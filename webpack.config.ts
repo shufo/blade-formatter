8f8077e54176555a756c4e4c2d51c0bcf79dd429
shufo <meikyowise@gmail.com>
2022-01-29 11:55:47 +0900

    refactor: 💡 delete unnecessary files

--- webpack.config.ts
+++
@@ -1,7 +0,0 @@
-import * as webpack from 'webpack';
-import CliConfig from './webpack.cli.config';
-import MainConfig from './webpack.main.config';
-
-const configurations: Array<webpack.Configuration> = [CliConfig, MainConfig];
-
-export default configurations;
