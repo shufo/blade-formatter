69516358a1de94c7a389d9e60e30aa3d19300523
shufo <meikyowise@gmail.com>
2022-05-04 03:56:03 +0900

    fix: 🐛 livewire tag causes unexpected formatting
    ✅ Closes: https://github.com/shufo/prettier-plugin-blade/issues/65

--- __tests__/formatter.test.ts
+++ __tests__/formatter.test.ts
@@ -3096,4 +3096,29 @@ describe('formatter', () => {
 
     await util.doubleFormatCheck(content, expected);
   });
+
+  test('livewire tag', async () => {
+    const content = [
+      `<div class="mt-6">`,
+      `    @foreach ($this->relations as $k => $relation )`,
+      `    <div x-show="tab == '#tab{{$k}}'" x-cloak>`,
+      `        <livewire:widgets.invoice-document-consumption.card :invoice_document_id="$this->     invoiceDocument->id" />`,
+      `    </div>`,
+      `    @endforeach`,
+      `</div>`,
+    ].join('\n');
+
+    const expected = [
+      `<div class="mt-6">`,
+      `    @foreach ($this->relations as $k => $relation)`,
+      `        <div x-show="tab == '#tab{{ $k }}'" x-cloak>`,
+      `            <livewire:widgets.invoice-document-consumption.card :invoice_document_id="$this->invoiceDocument->id" />`,
+      `        </div>`,
+      `    @endforeach`,
+      `</div>`,
+      ``,
+    ].join('\n');
+
+    await util.doubleFormatCheck(content, expected);
+  });
 });
