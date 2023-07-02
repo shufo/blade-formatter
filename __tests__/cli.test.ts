0f55f6a77786d947a09e0af98658d0d64b0e9d38
Shuhei Hayashibara <meikyowise@gmail.com>
2023-07-02 20:58:23 +0900

    test: 💍 allow 8s for test stability

--- __tests__/cli.test.ts
+++ __tests__/cli.test.ts
@@ -547,7 +547,7 @@ describe('The blade formatter CLI', () => {
     const startTime = performance.now();
     await util.checkIfTemplateIsFormattedTwice(input, target);
     const endTime = performance.now();
-    expect(endTime - startTime).toBeLessThan(7500);
+    expect(endTime - startTime).toBeLessThan(8000);
   });
 
   test.concurrent('respect tailwind.config.js automatically', async () => {
