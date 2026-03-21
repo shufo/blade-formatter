f555efb2255e61d2ef4ae847639da3f11997aa12
Shuhei Hayashibara <me@shufo.dev>
2026-03-21 19:51:51 +0900

    test: 💍 specify vitest alias explicitly

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,12 @@
+import path from "path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+	resolve: {
+		alias: {
+			src: path.resolve(__dirname, "./src"),
+		},
+	},
 	test: {
 		testTimeout: 10000,
 	},
